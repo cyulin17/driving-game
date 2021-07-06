7882a9303163d135a39d62841b56eaf7903c6556
ChiaYu Lin <lin.1008@osu.edu>
2021-07-06 13:17:43 -0700

    Completed issue-5-user-can-steer-their-car.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var $car = document.querySelector('img');
+var $carBox = document.querySelector('.car');
 
 var carDirections = [
   {
@@ -29,6 +30,56 @@ document.addEventListener('keydown', function (e) {
     if (e.key === carDirections[i].key) {
       $car.className = carDirections[i].direction;
     }
+
+    if (carLocation.carStarted === false) {
+      carLocation.carStarted = true;
+      timer = setInterval(carSteer, 16);
+    } else {
+      carLocation.carStarted = false;
+      clearInterval(timer);
+    }
+  }
+});
+
+var carLocation = {
+  x: 0,
+  y: 0,
+  carStarted: false
+};
+
+var timer;
+document.addEventListener('keydown', function (e) {
+
+  if (e.key === ' ') {
+    if (carLocation.carStarted === false) {
+      carLocation.carStarted = true;
+      timer = setInterval(carSteer, 16);
+    } else {
+      carLocation.carStarted = false;
+      clearInterval(timer);
+    }
+  }
+});
+
+function carSteer() {
+
+  if ($car.className === 'right') {
+    carLocation.x += 10;
+    $carBox.style.left = carLocation.x + 'px';
   }
 
-});
+  if ($car.className === 'up') {
+    carLocation.y -= 10;
+    $carBox.style.top = carLocation.y + 'px';
+  }
+
+  if ($car.className === 'left') {
+    carLocation.x -= 10;
+    $carBox.style.left = carLocation.x + 'px';
+  }
+
+  if ($car.className === 'down') {
+    carLocation.y += 10;
+    $carBox.style.top = carLocation.y + 'px';
+  }
+}
